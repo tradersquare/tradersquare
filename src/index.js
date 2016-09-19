ea4397e0e4f7950fd5c040545e1a6b89d1a92a32
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-09-19 16:14:04 -0700

    [bug-fix](init)(client): fix json lint script. change to most recent npm packages

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
+import routes from './routes';
+import promise from 'redux-promise';
+import App from './components/app';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
