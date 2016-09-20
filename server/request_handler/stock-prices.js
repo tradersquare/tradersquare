554cdff8007a27c0bb1aba4fdcc2ed818476985d
Angelina <angelina.may@hotmail.com>
2016-09-20 13:59:55 -0700

    [feature](server):set up promises to request data from Intrinio

--- server/request_handler/stock-prices.js
+++ server/request_handler/stock-prices.js
@@ -4,6 +4,46 @@ const username = process.env.INTRINIO_USER;
 const password = process.env.INTRINIO_PASSWORD;
 const intrinio = require(path.resolve( __dirname, "intrinio"))(username, password)
 
-module.exports = (ticker) => {
+let element = {};
+
+const statementPromise = (ticker) => {
+  return new Promise((resolve, reject) => {
+    intrinio.statement(ticker, 'income_statement', '2014', 'FY')
+      .on('complete', (data, response) => {
+        const results = data.data;
+        for(let i of results){
+          element[i.tag] = i.value;
+        }
+        console.log(element);
+        resolve(element);
+      })
+      .on('error', (error) => {
+        reject(error);
+      })
+  })
+}
+
+
+
+module.exports = (ticker, res) => {
+
+  Promise.all([
+    statementPromise(ticker)
+    ])
+  .then((something)=>{
+
+    // const results = data.data;
+    // for(let i of results){
+    //   element[i.tag] = i.value;
+    // }
+    console.log(something);
+    res.send(something);
+
+  })
+  .catch(err => {
+    throw err;
+  })
+
   
+
 }
