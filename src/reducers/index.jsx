b1997f0dd406f2540033201c21cd4218a565a11a
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-09-20 08:13:05 -0700

    [feature](client): add strategy placeholder

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import StockReducer from './stock_reducer';
+import Strat1 from './strategyF_reducer';
 
 const rootReducer = combineReducers({
-  stock: StockReducer
+  stock: StockReducer,
+  strat1: Strat1
 });
 
 export default rootReducer;
