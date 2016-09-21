f0089d0623c9530ae818fa5d0d56aa083763a8eb
Battenfield <chrisbattenfield@gmail.com>
2016-09-20 17:07:20 -0700

    [feature](client):create serverReq to get data from the server to the client

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -7,7 +7,13 @@ export const GET_STRAT = 'GET_STRAT';
 
 export function getData() {
   // const request = axios.get('/moreStocks')
-
+  let serverReq = axios.post('/stockData', {"ticker": "FB"})
+  .then(function(res) {
+    console.log('res::::: ',res);
+  })
+  .catch(function(err) {
+    console.log('errrrrrrr', err);
+  });
   return {
     type: GET_DATA,
     payload: request
@@ -15,8 +21,18 @@ export function getData() {
 }
 
 export function searchStockData() {
+  let serverReq = axios.post('/stockData', {"ticker": "FB"})
+  .then(function(res) {
+    console.log('res::::: ',res);
+  })
+  .catch(function(err) {
+    console.log('errrrrrrr', err);
+  });
   console.log('search my nodes')
-
+  return {
+    type: GET_DATA,
+    payload: serverReq
+  };
   //get request to server goes here
 
   //return {
