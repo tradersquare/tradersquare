30fbc25c3914b3e5bfc6c555e9d13a0081dba06f
Angelina <angelina.may@hotmail.com>
2016-09-22 15:08:24 -0700

    [feature](client):retrieve data for strategy view from server

--- src/actions/watchlist_Add.jsx
+++ src/actions/watchlist_Add.jsx
@@ -5,20 +5,14 @@ export const ADD = "ADD";
 export const GET_STRAT_DATA = "GET_STRAT_DATA"
 
 export default function() {
-  // const tmpStock = ['FB', 'GOOG', 'TWTR', 'AMZN', 'TSLA', 'SCTY', 'F', 'NOK', 'SBUX', 'YHOO', 'AAPL', 'JPM'];
-  // axios.all(tmpStock.map((ticker) => axios.get(`/stockDataTmp/${ticker}`)))
-  //   .then(axios.spread((...data) => {
-  //     console.log(".then", data[0].data.data)
-  //     return {
-  //       type: GET_STRAT_DATA,
-  //       payload: {}
-  //     }
-  //   }));
-
-  let test = axios.get('/stockDataTmp/FB')
-  console.log('in actions')
-  return {
-    type: GET_STRAT_DATA,
-    payload: test
-  }
+  const tmpStock = ['FB', 'GOOG', 'TWTR', 'AMZN', 'TSLA', 'SCTY', 'F', 'NOK', 'SBUX', 'YHOO', 'AAPL', 'JPM'];
+  return axios.all(tmpStock.map((ticker) => axios.get(`/stockDataTmp/${ticker}`)))
+    .then(axios.spread((...data) => {
+      console.log(".then", data[0].data.data)
+      let stockInfo = data.map((stock) => stock.data.data)
+      return {
+        type: GET_STRAT_DATA,
+        payload: stockInfo
+      }
+    }));
 }
