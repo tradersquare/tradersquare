8ce4775c361e31eb78d9a5df1a162e0908a4e7b4
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-09-22 13:16:12 -0700

    [bug-fix](init): Remove publicpath in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack')
+const path = require('path');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
     path: path.join(__dirname, 'public'),
-    publicPath: '/public/',
     filename: 'bundle.js'
   },
   externals: {
