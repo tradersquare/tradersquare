1515321702c3ae28c724c36c2fe56ba0ae969270
Battenfield <chrisbattenfield@gmail.com>
2016-09-22 15:36:10 -0700

    [feature](client): render basic strategyview list

--- src/components/watchlist.jsx
+++ src/components/watchlist.jsx
@@ -5,14 +5,24 @@ import {bindActionCreators} from 'redux';
 import getStratData from '../actions/watchlist_Add'
 
 class WatchList extends Component {
-  handleClick () {
-    this.props.getStratData
-  }
 
   render(){
+    console.log('straaaaaaat', this.props.strategyData);
+    let stratData = this.props.strategyData.sort((a,b) => {
+      if(a.altmanzscore > b.altmanzscore) return -1;
+      if(a.altmanzscore < b.altmanzscore) return 1;
+    })
+    stratData = this.props.strategyData.map((stock) => {
+      return (
+      <tr>
+          <td>{stock.ticker}</td>
+          <td>{stock.name}</td>
+          <td>{stock.altmanzscore}</td>
+      </tr>)
+    })
     return (
         <div>
-        <button onClick={this.getStratData}>something</button>
+          <table>{stratData}</table>
         </div>
 
       )
@@ -22,7 +32,7 @@ class WatchList extends Component {
 function mapStateToProps(state) {
   console.log('mapStateToProps state: ', state);
   return {
-    stockData: "something"
+    strategyData: state.watchlist
   }
 }
 
