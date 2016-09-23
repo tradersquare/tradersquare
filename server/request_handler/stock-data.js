176a1c77b77a094fe7364204c72c0657eb633099
Cindy Sun <c.s.sun5@gmail.com>
2016-09-22 19:39:16 -0700

    [feature](db): configure table columns

--- server/request_handler/stock-data.js
+++ server/request_handler/stock-data.js
@@ -25,7 +25,7 @@ const statementPromise = (ticker, statement, year, period) => {
 
 const dataPointPromise = (ticker) => {
   return new Promise((resolve, reject) => {
-    intrinio.data_point(ticker, "52_week_high,52_week_low,marketcap,pricetoearnings,basiceps,volume,average_daily_volume,open_price,close_price,change,beta")
+    intrinio.data_point(ticker, "fiftytwo_week_high,fiftytwo_week_low,marketcap,pricetoearnings,basiceps,volume,average_daily_volume,open_price,close_price,change,beta")
       .on('complete', (data, response) => {
         const results = data.data;
         for(let i of results){
@@ -68,26 +68,43 @@ const historicalPricePromise = (ticker) => {
   });
 };
 
+module.exports.inputCols;
 
 module.exports.stockData = (ticker, res) => {
 
   Promise.all([
-    // statementPromise(ticker, "income_statement", "2014", "FY"),
-    // statementPromise(ticker, "income_statement", "2015", "FY"),
-    // statementPromise(ticker, "balance_sheet", "2014", "FY"),
-    // statementPromise(ticker, "balance_sheet", "2015", "FY"),
-    // statementPromise(ticker, "cash_flow_statement", "2014", "FY"),
-    // statementPromise(ticker, "cash_flow_statement", "2015", "FY"),
-    // statementPromise(ticker, "calculations", "2014", "FY"),
-    // statementPromise(ticker, "calculations", "2015", "FY"),
+    statementPromise(ticker, "income_statement", "2014", "FY"),
+    statementPromise(ticker, "income_statement", "2015", "FY"),
+    statementPromise(ticker, "balance_sheet", "2014", "FY"),
+    statementPromise(ticker, "balance_sheet", "2015", "FY"),
+    statementPromise(ticker, "cash_flow_statement", "2014", "FY"),
+    statementPromise(ticker, "cash_flow_statement", "2015", "FY"),
+    statementPromise(ticker, "calculations", "2014", "FY"),
+    statementPromise(ticker, "calculations", "2015", "FY"),
     dataPointPromise(ticker),
     zscorePromise(ticker)
     ])
   .then((data) => {
-    db.query('SELECT * from productionschema.stockdatatable;')
-    .on('row', function(row) {
-      console.log("row", row);
+    let indexedElements = [];
+    for (let key in element) {
+      indexedElements.push(key);
+    }
+    const sortedElements = indexedElements.sort();
+
+    let dtCols = '';
+    const varChar = 'varchar(20)';
+    sortedElements.forEach((val) => {
+      dtCols = dtCols + val+' '+varChar + ', ';
     });
+
+    inputCols = dtCols.slice(0, dtCols.length - 2);
+
+    console.log(inputCols);
+
+    // db.query('SELECT * from productionschema.stockdatatable;')
+    // .on('row', function(row) {
+    //   console.log("row", row);
+    // });
     res.send(element);
   })
   .catch(err => {
