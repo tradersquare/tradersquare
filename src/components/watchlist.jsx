fd63c9f42b473aba7e3779d890fca4e87b4a1f06
Battenfield <chrisbattenfield@gmail.com>
2016-09-22 17:07:31 -0700

    [feature](client): Display strategy data for multiple strategies

--- src/components/watchlist.jsx
+++ src/components/watchlist.jsx
@@ -5,24 +5,59 @@ import {bindActionCreators} from 'redux';
 import getStratData from '../actions/watchlist_Add'
 
 class WatchList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {selectValue: 'altmanzscore'}
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+  handleChange(event) {
+    this.setState({selectValue: event.target.value})
+  }
 
   render(){
+    let currentStrat = this.state.selectValue;
     console.log('straaaaaaat', this.props.strategyData);
     let stratData = this.props.strategyData.sort((a,b) => {
-      if(a.altmanzscore > b.altmanzscore) return -1;
-      if(a.altmanzscore < b.altmanzscore) return 1;
+      if(a[currentStrat] > b[currentStrat]) return -1;
+      if(a[currentStrat] < b[currentStrat]) return 1;
     })
+    let stockKey = 0;
+
     stratData = this.props.strategyData.map((stock) => {
+      stockKey++;
       return (
-      <tr>
+      <tr key={stockKey}>
           <td>{stock.ticker}</td>
           <td>{stock.name}</td>
-          <td>{stock.altmanzscore}</td>
+          <td>{stock[currentStrat].toFixed(3)}</td>
+          <td>{100-(Math.round((stockKey/this.props.strategyData.length)*100))}%</td>
       </tr>)
     })
     return (
         <div>
-          <table>{stratData}</table>
+          <select
+            value={this.state.selectValue}
+            onChange={this.handleChange}
+          >
+            <option value="altmanzscore">Z-Score</option>
+            <option value="assetturnover">Asset Turnover</option>
+            <option value="grossmargin">Gross Margin</option>
+            <option value="pricetoearnings">P/E</option>
+            <option value="currentratio">Current Ratio</option>
+            <option value="epsgrowth">EPS Growth</option>
+          </select>
+          <table>
+          <tr>
+            <th>Ticker</th>
+            <th>Name</th>
+            <th>Value</th>
+            <th>Percentile</th>
+          </tr>
+          {stratData}
+          </table>
         </div>
 
       )
