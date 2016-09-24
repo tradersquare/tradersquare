f129da49122a5432a76dcd94423b494aec18fbbf
Battenfield <chrisbattenfield@gmail.com>
2016-09-23 20:06:03 -0700

    [feature](client): Add landing page and configure routes

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -5,12 +5,12 @@ import StockView from './stock_view';
 
 
 
-export default class App extends Component {
+export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1 className="heading">TraderSquare</h1>
-        {this.props.children}
+        <h1 className="heading"></h1>
+        <SearchBar />
       </div>
     );
   }
