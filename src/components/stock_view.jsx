ffaa79dbbb1c56b8a410baa9dfa7a217dac1e139
Angelina <angelina.may@hotmail.com>
2016-09-23 19:13:22 -0700

    [style](client): add basic styling

--- src/components/stock_view.jsx
+++ src/components/stock_view.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
+import SearchBar from './search_bar';
+
 
 class StockView extends Component {
 
   render() {
+    <SearchBar />
     console.log('mapStateToProps rerender: ', this.props.stockData);
     let stockData = this.props.stockData.map((val) => {
       return (<div key={val}><span>{val[0]}</span> : <span>{val[1]}</span></div>);
