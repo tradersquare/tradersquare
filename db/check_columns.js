9e1251779a6249dba64e4546b23f8d3255139c44
Cindy Sun <c.s.sun5@gmail.com>
2016-09-29 15:04:41 -0700

    [bug-fix](db): change table name so column comparison works

--- db/check_columns.js
+++ db/check_columns.js
@@ -1,7 +1,7 @@
 const db = require('./config.js');
 
 module.exports = (res, results, resolve) => {
-  db.query(`SELECT column_name from information_schema.columns WHERE table_schema = 'productionschema' AND table_name = 'stockdatatable';`)
+  db.query(`SELECT column_name from information_schema.columns WHERE table_schema = 'productionschema' AND table_name = 'realdata';`)
   .on('row', row => {
     results.push(row.column_name);
   })
