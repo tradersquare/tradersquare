d68241c2c2a0959ec13331ce428b3135ce66d71a
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-09-29 15:11:11 -0700

    [refactor](client): remove global vars in reducers. using state.

--- src/reducers/stock_reducer.jsx
+++ src/reducers/stock_reducer.jsx
@@ -1,23 +1,19 @@
 import { GET_DATA } from '../actions/stock_search';
 
-const INITIAL_STATE = { all: [], post: null};
-
 /**
  * [description]
 * @param  {[type]} action                [description]
 * @functino        action                [make get call to server]
  */
-let myDefault = [];
 
-export default function(state = INITIAL_STATE, action) {
+export default function(state = [], action) {
   // console.log()
   switch(action.type) {
     case GET_DATA:
       console.log('reducer: stock_reducer: action.payload ', action.payload);
-      myDefault = (action.payload.data.data);
       return action.payload.data.data;
     default:
       console.log('did it work? (myDefault): ', myDefault);
-      return myDefault;
+      return state;
   }
 }
