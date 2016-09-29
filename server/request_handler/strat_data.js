8ff63dc92b40b146387fdbc26de7bb72e878c4ef
Angelina <angelina.may@hotmail.com>
2016-09-28 19:54:25 -0700

    [feature](client): format data for stock view

--- server/request_handler/strat_data.js
+++ server/request_handler/strat_data.js
@@ -3,33 +3,17 @@ const restler = require('restler');
 const username = process.env.INTRINIO_USER;
 const password = process.env.INTRINIO_PASSWORD;
 const intrinio = require(path.resolve( __dirname, "intrinio"))(username, password);
-
-let element = {};
-
-const calculationPromise = (ticker) => {
-  return new Promise((resolve, reject) => {
-    intrinio.data_point(ticker, "ticker,name,altmanzscore,assetturnover,grossmargin,pricetoearnings,currentratio,epsgrowth,divpayoutratio,debttoequity,enterprisevalue,earningsyield,netincomegrowth,roe,52_week_high,52_week_low,marketcap,pricetoearnings,basiceps,volume,average_daily_volume,open_price,close_price,change,beta,short_description")
-      .on('complete', (data, response) => {
-        const results = data.data;
-        for(let i of results){
-          element[i.item] = i.value;
-        }
-        resolve(element);
-      })
-      .on('error', (error) => {
-        reject(error);
-      })
-  });
-};
+const apiReq = require('./api_req.js');
 
 module.exports = (ticker, res) => {
 
   Promise.all([
-    calculationPromise(ticker),
+    apiReq('data_point', ticker, "ticker,name,52_week_high,52_week_low,marketcap,basiceps,volume,average_daily_volume,open_price,close_price,change,beta,short_description"),
+    apiReq('financials', ticker)
     ])
   .then((data) => {
-    console.log("******", element)
-    res.send({data: element});
+    let flatData = data.reduce( (prev, curr) => Object.assign(prev, curr));
+    res.send({data: flatData});
   })
   .catch(err => {
     throw err;
