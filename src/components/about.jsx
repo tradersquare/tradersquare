3585344397ff7e029b9899efcfef870e32949903
angelinacodes <angelinasrhapsody@gmail.com>
2016-10-06 16:10:19 -0700

    [bug-fix](client): resolve stash conflict

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import {bindActionCreators} from 'redux';
-
+import Header from './header'
 
 
 class About extends Component {
