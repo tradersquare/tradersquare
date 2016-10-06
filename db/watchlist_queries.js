c2cc8fbd0646cb30b30c0c378de7aa617ef2316d
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-06 14:37:54 -0700

    [docs](db): explain how to use watchlist db table

--- db/watchlist_queries.js
+++ db/watchlist_queries.js
@@ -3,15 +3,15 @@ const db = require('./config.js');
 module.exports.watchlistInsert = (res, obj) => {
   const startInsertQuery = `INSERT INTO watchlistSchema.watchlistTable`;
   const queryCols = `(userExtId, StockName, StockTicker, ClosingPrice)`
-  const queryVals = `VALUES ('Akul', 'Facebook Inc.', 'FB', 1000)`;
+  const queryVals = `VALUES (45, 'Facebook Inc.', 'FB', 1000)`;
 
   const insertQuery = `${startInsertQuery} ${queryCols} ${queryVals}`;
   console.log('final insertQuery: ', insertQuery);
-  // db.query(insertQuery)
-  //   .on('end', function() {
-  //     console.log("created")
-  //   })
-  //   .catch(console.error)
+  db.query(insertQuery)
+    .on('end', function() {
+      console.log("added")
+    })
+    .catch(console.error)
   res.end();
 }
 
@@ -30,3 +30,14 @@ function queryWatchlistRow() {
 }
 
 module.exports.queryWatchlistRow = queryWatchlistRow;
+
+/*From postgres terminal:
+to get into postgres DB, must have postgres installed on local machine
+need entry username and password (ask developers)
+to get all entries for this table: [SELECT * FROM watchlistSchema.watchlistTable;]
+
+to see all schemas: [\dn;]
+to see all tables in public: [\d]
+
+
+*/
