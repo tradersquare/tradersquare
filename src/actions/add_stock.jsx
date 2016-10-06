db994aa9bbe74c99a53fa8dadab637d8d0d4bcf7
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-06 16:30:22 -0700

    [feature](client): add logic to check if watchlist already has entry

--- src/actions/add_stock.jsx
+++ src/actions/add_stock.jsx
@@ -2,16 +2,26 @@ import axios from 'axios';
 
 export const ADD_STOCK = "ADD_STOCK";
 
-export default function(stock) {
-  // console.log('add_stock action: ', stock);
+export default function(stock, watchlistData) {
   // debugger;
-  const addToDB = axios.post('/addToWatchlist', {
+  let insideWatchlist = true;
+  watchlistData.forEach( v => {
+    if (v.ticker === stock.ticker) {
+      insideWatchlist = false;
+    }
+  })
+
+  if (insideWatchlist) {
+    const addToDB = axios.post('/addToWatchlist', {
       userExtId: 45,
       StockTicker: stock.ticker,
       StockName: stock.name,
       ClosingPrice: stock.close_price
     })
-    .then(console.log('went through'));
+      .then( () => {
+        console.log('went through, now do get req'));
+      }
+  }
 
 
   return {
--- src/components/stock_view.jsx
+++ src/components/stock_view.jsx
@@ -93,7 +93,7 @@ class StockView extends Component {
     // ev.preventDefault();
     let stockData = this.props.stockData;
     console.log('handleAdd :', stockData);
-    this.props.AddStock(stockData);
+    this.props.AddStock(stockData, this.props.watchlistData);
   }
 
   render() {
@@ -213,6 +213,7 @@ function mapStateToProps(state) {
     graphData: state.graphData,
     percentileData: state.percentileData,
     sentimentData: state.sentimentData
+    watchlistData: state.watchList
   }
 }
 
