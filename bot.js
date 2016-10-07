7bad4dd624b5a4b5384e2d448a0d814db85d0f46
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-07 10:43:21 -0700

    [deploy]: change cron time to 47, 4 min in future

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
 module.exports = {
   start: function() {
+<<<<<<< HEAD
       console.log('cron');
+=======
+    //put here dummy insert into table, test in terminal first
+      console.log('cron');
+
+    db.query(` INSERT INTO dummy.cron (var, numnum) VALUES ('hello', 45);`)
+      .on('end', function() {
+        console.log('check dummy yo');
+      })
+>>>>>>> [deploy]: change cron time to 47, 4 min in future
   }
 };
