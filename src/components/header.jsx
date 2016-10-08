a66f95e9acee0c1141284b22a5205b2d5a6a13f0
angelinacodes <angelinasrhapsody@gmail.com>
2016-10-07 22:49:09 -0700

    [bug-fix](client): fix secondary search rendering

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -28,6 +28,9 @@ export default class Header extends Component {
           <li className="nav-item">
             <Link className="nav-link" to="/about">About</Link>
           </li>
+          <li className="nav-item">
+            <LoginNav />
+          </li>
         </ul>
         </div>
 
