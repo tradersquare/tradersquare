21d4c0c39fad61e3f055c513f90130b71b184eeb
Battenfield <chrisbattenfield@gmail.com>
2016-10-08 15:34:35 -0700

    [test](server): checking to see if testing suite works

---
+++ test/server_tests.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const should = require('should');
+const http = require('http');
+
+describe('Server testing', () => {
+  describe('1 to 1', () => {
+    it('true should be true', () => {
+      assert.equal(true, true);
+  });
+});
+});
