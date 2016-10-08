4e785b9e96c11b55b34040fe6feb58589907d27f
angelinacodes <angelinasrhapsody@gmail.com>
2016-10-08 11:41:50 -0700

    [style](client): reformat landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -12,21 +12,69 @@ export default class Landing extends Component {
     return (
       <div>
       <Header />
-      <div className="landing-container">
+      <div className=" row landing-container">
 
           <div className="landing-input">
-          <h1 className="landing-header">TraderSquare</h1>
+          <h1 className="">picking stocks has never been easier</h1>
+
             <div className="col-md-3"></div>
             <div className="col-md-6">
-              <SearchBar/>
               <br/>
-              <StratNav/>
               <br/>
-              <FilterNav/>
             </div>
             <div className="col-md-3"></div>
 
           </div>
+
+      </div>
+
+      <div className="row">
+
+        <div className="col-md-4">
+          <h2 className="centerheading">SEARCH STOCKS</h2>
+
+          <div className="col-md-12"><p className="col-md-12 centertext">type in a ticker symbol to learn more about the stock. 
+
+          add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more  add more </p></div>
+
+          <div className="row col-md-12">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+              <SearchBar/>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+
+        </div>
+
+        <div className="col-md-4">
+          <h2 className="centerheading">EXPLORE BY METRIC</h2>
+          
+          <div className="col-md-12"><p className="col-md-12 centertext">about strat view about strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat viewabout strat view</p></div>
+
+          <div className="row col-md-12">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <button className="btn btn-secondary"><StratNav/></button>
+            </div>
+            <div className="col-md-4"></div>
+          </div>
+
+        </div>
+
+        <div className="col-md-4">
+          <h2 className="centerheading">PICK STOCKS</h2>
+          <div className="col-md-12"><p className="col-md-12 centertext">about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view about filter view</p></div>
+          
+          <div className="row col-md-12">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <button className="btn btn-secondary"><FilterNav/></button>
+            </div>
+            <div className="col-md-4"></div>
+          </div>
+
+        </div>
 
       </div>
       </div>
