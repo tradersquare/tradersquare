0e1fb0f709f82f22af8c672586ffe647bfd22e94
Battenfield <chrisbattenfield@gmail.com>
2016-10-08 15:48:54 -0700

    [test](client): implement testing for stock reducer and add dependencies

--- test/reducer-test.js
+++ test/reducer-test.js
@@ -4,15 +4,21 @@ import { expect } from 'chai';
 import { GET_DATA } from '../src/actions/stock_search';
 import reducer from '../src/reducers/stock_reducer';
 
-describe('Stock Reducer Tests', () => {
+describe('Reducers testing', () => {
   let state, action;
   beforeEach(() => {
     state = ['stock state'];
     action = {
       type: GET_DATA,
       payload: {
-        data: 'stock data';
+        data: 'stock data'
       }
     };
+  })
+  it('should be a function', () => {
+    expect(reducer).to.be.a('function');
+  })
+  it('should return the current state by default', () => {
+    expect(reducer(state, 'stock')).to.eql(['stock state']);
   })
 });
