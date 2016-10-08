72e3ac2fc672c224776e195f20647637ff9ed24e
Battenfield <chrisbattenfield@gmail.com>
2016-10-08 15:36:12 -0700

    [test](server): create test to see if server is up and running

--- test/server_tests.js
+++ test/server_tests.js
@@ -6,6 +6,15 @@ describe('Server testing', () => {
   describe('1 to 1', () => {
     it('true should be true', () => {
       assert.equal(true, true);
+    });
   });
-});
+
+  describe('Server up and running', () => {
+    it('Should be a 200 code', (done) => {
+      http.get('http://localhost:3000', (res) => {
+        assert.equal(200, res.statusCode);
+        done();
+      });
+    });
+  });
 });
