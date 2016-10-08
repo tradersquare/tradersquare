2c015e673cfaf50881f43527acc08e8d5a11dc8a
Battenfield <chrisbattenfield@gmail.com>
2016-10-08 15:40:32 -0700

    [test](client): setup testing for stock reducer

---
+++ test/handlers-test.js
@@ -0,0 +1,23 @@
+// const assert = require('assert');
+// const should = require('should');
+// const http = require('http');
+//
+// describe('Testing Test', () => {
+// describe('1 to 1', () => {
+//     it('true should be true', () => {
+//       assert.equal(true, true);
+//   });
+// });
+//
+// describe('Server Up', () => {
+//   it('Should be a 200 code', (done) => {
+//     http.get('http://localhost:3000', (res) => {
+//       assert.equal(200, res.statusCode);
+//       console.log('herererere' ,res.data)
+//       done();
+//     });
+//   });
+// });
+//
+//
+// });
