d0867bcd3263ae39ca1778cd2aff7e8a2b8f2af9
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-08 17:30:42 -0700

    [deploy]: remove console.log for each data change to DB

--- server/request_handler/db_updater.js
+++ server/request_handler/db_updater.js
@@ -26,12 +26,13 @@ module.exports = (ticker) => {
       compData.forEach( (v, i) => {
         let col = v.tag;
         let newValue = v.value;
-        if (col === 'marketcap') {
-          newValue = 1000000000;
-        }
-        db.query(`UPDATE backup.stockdatatable SET ${col} = ${newValue} WHERE ticker = '${ticker}';`)
+        // if (col === 'marketcap') {
+        //   // newValue = 1000000000;
+        // }
+        db.query(`UPDATE backup.stockdatatable SET marketcap = ${newValue} WHERE ticker = '${ticker}';`)
+        // db.query(`UPDATE backup.stockdatatable SET ${col} = ${newValue} WHERE ticker = '${ticker}';`)
         .on('end', () => {
-          console.log('data changed', i, ticker);
+          // console.log('data changed', i, ticker);
         })
         .catch( console.error)
       })
