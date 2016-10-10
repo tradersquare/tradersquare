add7ce7999ad567ee0c55a4f3687dddacd8cf97f
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-10 09:45:06 -0700

    [style](client}: shorten landing page description. fix typo

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -33,7 +33,7 @@ export default class Landing extends Component {
         <div className="col-md-4">
           <h2 className="centerheading">SEARCH STOCKS</h2>
 
-          <div className="col-md-12"><p className="col-md-12 centertext">Type in any ticker symbol to learn about the stock. We have compiled the most common metrics and breif descriptions for each.</p></div>
+          <div className="col-md-12"><p className="col-md-12 centertext">Enter any stock ticker to see it's most common metrics and breif descriptions for each metric.</p></div>
 
           <div className="row col-md-12">
             <div className="col-md-1"></div>
@@ -47,8 +47,8 @@ export default class Landing extends Component {
 
         <div className="col-md-4">
           <h2 className="centerheading">EXPLORE BY METRIC</h2>
-          
-          <div className="col-md-12"><p className="col-md-12 centertext">Check out our Leaderboard to see the best performing stocks in the S&P 500 based on your favorite metrics.</p></div>
+
+          <div className="col-md-12"><p className="col-md-12 centertext">See the best performing stocks in the S&P 500 based on your favorite metrics.</p></div>
 
           <div className="row col-md-12">
             <div className="col-md-4"></div>
@@ -62,8 +62,8 @@ export default class Landing extends Component {
 
         <div className="col-md-4">
           <h2 className="centerheading">PICK STOCKS</h2>
-          <div className="col-md-12"><p className="col-md-12 centertext">You can also pick stocks with your own set of criterias. Check out our stock picker, which includes data for the S&P 500.</p></div>
-          
+          <div className="col-md-12"><p className="col-md-12 centertext">Pick stocks with your own set of criteria, with data from the S&P 500.</p></div>
+
           <div className="row col-md-12">
             <div className="col-md-4"></div>
             <div className="col-md-4">
